04cdc5a74c26d21dd73a2b960857732102f1b7f9
samirls <samirlaguardiaii@gmail.com>
2023-06-02 19:43:33 -0300

    small changes

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 3- Ainda não consegui utilizar o select do shadcn
 
 4- fazer (rota?) para puxar todos os usuários e mostrar na pagina inicial
+
+5- da pra ter vários botoes com varias cores diferentes ou só primary e secondary, etc?
--- app/tarefas/page.tsx
+++ app/tarefas/page.tsx
@@ -6,20 +6,17 @@ import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
 
 function Tarefas() {
-  
   const { data: session } = useSession();
 
   return (
     <div className={styles.container1}>
       <div className={styles.container2}>
-        <h1 className={styles.title}>
-          Tarefas da Família {session?.user.email}
-        </h1>
-        <h1 className={styles.title2}>Olá {session?.user.email}</h1>
-
-        <Input type="text" placeholder="Escreva algo a ser feito" />
-
-        <Button>Adicionar</Button>
+        <h1 className={styles.title}>Tarefas da Família</h1>
+        <div>
+          <Input type="text" placeholder="Escreva algo a ser feito" />
+          <Button>Adicionar</Button>
+        </div>
+        
       </div>
     </div>
   );
